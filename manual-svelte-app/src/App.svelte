b1d7d7b567e022fbf0fbd39c5d1147617a4d5268
tarnas14 <tarnas14@gmail.com>
2019-10-03 21:01:20 +0200

    [manual-svelte-app] you can add multiple named sessions and they are on css-grid

--- manual-svelte-app/src/App.svelte
+++ manual-svelte-app/src/App.svelte
@@ -2,7 +2,8 @@
   import {startSessionFactory, getSessionFactory, registerGameResultFactory} from 'no-self-control'
   import Session from './Session.svelte'
 
-  export let name;
+  let name = 'session'
+  let sessionError = ''
   let repo = {}
   $: sessions = Object.keys(repo)
 
@@ -15,16 +16,55 @@
 
   const startSession = startSessionFactory({sessionRepo: svelteRepo})
   const getSession = getSessionFactory({sessionRepo: svelteRepo})
-  const registerGameResult = gameResult => registerGameResultFactory({sessionRepo: svelteRepo})('some name', gameResult)
+  const registerGameResult = (sessionName, gameResult) => registerGameResultFactory({sessionRepo: svelteRepo})(sessionName, gameResult)
 
   async function start() {
-    await startSession('some name')
+    try {
+      await startSession(name)
+      name = ''
+      sessionError = ''
+    } catch (error) {
+      console.log(error.message)
+      sessionError = error.message
+    }
   }
 </script>
 
-{#if !sessions.length}
-  <button on:click={start}>start</button>
-{/if}
-{#each sessions as session (session)}
-  <Session session={repo[session]} {registerGameResult}/>
-{/each}
+<style>
+  .App {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: 50px 30px repeat(3, 1fr);
+
+    width: 1000px;
+    margin: 20px auto;
+  }
+
+  .session-starter {
+    grid-row: 1 / 2;
+    grid-column: 2 / -2;
+
+    display: flex;
+    justify-content: center;
+  }
+
+  .validation-errors {
+    grid-row: 2 / 3;
+    grid-column: 2 / -2;
+    text-align: center;
+  }
+</style>
+
+<div class="App">
+  <div class="session-starter">
+    <input bind:value={name}/>
+    <button on:click={start}>start</button>
+  </div>
+  <div class="validation-errors">
+    {sessionError}
+  </div>
+
+  {#each sessions as session (session)}
+    <Session session={repo[session]} registerGameResult={gameResult => registerGameResult(session, gameResult)}/>
+  {/each}
+</div>
--- manual-svelte-app/src/Session.svelte
+++ manual-svelte-app/src/Session.svelte
@@ -30,15 +30,24 @@
   }
 </script>
 
-<p>{session.name}</p>
-<p>HP: {session.hp}</p>
-<p>games: {session.gamesCount}</p>
-{#if warmingUp}
-  <p>relax, this is still warmup</p>
-{:else finished !finished}
-  <p>THE GAME IS AFOOT</p>
-{:else if finished}
-  <p>you're done, mate</p>
-{/if}
-<button disabled={finished} on:click={win}>WIN</button>
-<button disabled={finished} on:click={loss}>LOSS</button>
+<style>
+  .Session {
+    grid-column: span 2;
+    grid-row: span 3;
+  }
+</style>
+
+<div class="Session">
+  <p>{session.name}</p>
+  <p>HP: {session.hp}</p>
+  <p>games: {session.gamesCount}</p>
+  {#if warmingUp}
+    <p>relax, this is still warmup</p>
+  {:else if !finished}
+    <p>THE GAME IS AFOOT</p>
+  {:else if finished}
+    <p>you're done, mate</p>
+  {/if}
+  <button disabled={finished} on:click={win}>WIN</button>
+  <button disabled={finished} on:click={loss}>LOSS</button>
+</div>
